9fd32a52f800e3fe1045594d6b8cca432ec32788
jake-aro <arojake@gmail.com>
2022-05-17 17:17:09 +1200

    function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,8 +1,21 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
 
+var uni0 = document.getElementById('uni0')
+var uni1 = document.getElementById('uni1')
+var uni2 = document.getElementById('uni2')
 
-var
+uni0.onclick = clicked
+uni1.onclick = clicked
+uni2.onclick = clicked
+
+// figure out which unicorn I clicked
+// increase its inflation level
+//
+
+function clicked() {
+  console.log('I am unicorn')
+}
