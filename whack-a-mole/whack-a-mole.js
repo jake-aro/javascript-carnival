e842bf7500cf0d2778390c162921e8740925cbd9
jake-aro <arojake@gmail.com>
2022-05-17 22:50:56 +1200

    mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,21 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+// Pseudocodes
+//  get a random cell
+// mole image appear in random cell
+// click on the mole
+// mole respawns in another cell
+
+var cells = document.getElementsByTagName('TD')
+
+var randomIndex = Math.floor(Math.random() * cells.length);
+
+var randomCell = cells.[randomIndex]
+
+var mole = document.createElement("img");
+mole.src = "./mole.PNG";
+randomCell.appendChild(mole);
