be9d4fa42dbf3583dcafdaa3f7af74719e46beee
jake-aro <arojake@gmail.com>
2022-05-17 23:02:32 +1200

    mole audio

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -12,10 +12,24 @@ console.log('Whack-a-Mole!')
 
 var cells = document.getElementsByTagName('TD')
 
-var randomIndex = Math.floor(Math.random() * cells.length);
+var randomIndex = Math.floor(Math.random() * cells.length)
 
-var randomCell = cells.[randomIndex]
+var randomCell = cells[randomIndex]
 
-var mole = document.createElement("img");
-mole.src = "./mole.PNG";
-randomCell.appendChild(mole);
+var mole = document.createElement('img')
+
+mole.src = './mole.PNG'
+mole.id = 'mole'
+
+randomCell.appendChild(mole)
+
+mole.onclick = whackedMole
+
+function whackedMole() {
+  randomIndex = Math.floor(Math.random() * cells.length)
+  randomCell = cell[randomIndex]
+  randomCell.appendChild(mole)
+
+  var audio = new Audio('./whack-audio.wav')
+  audio.play()
+}
